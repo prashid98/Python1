e0c14b198eb792d6e01a77a89e35ad6293401941
prashid98 <prashid.kafle@cotiviti.com>
2022-04-04 20:24:30 +0545

    working on stack

---
+++ Data_structure/Stack_list_queue.py
@@ -0,0 +1,10 @@
+#initialization of stack
+stack=[]
+
+#adding element  in stack
+stack.append("element1")
+stack.append("element2")
+stack.append("element3")
+
+#printing the element of stack
+print(stack)
