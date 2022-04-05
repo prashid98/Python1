2a938d191aa6c7b193a0463d5552b4e2c2289bfb
prashid98 <prashid.kafle@cotiviti.com>
2022-04-05 13:27:00 +0545

    working on list

--- Data_structure/List.py
+++ Data_structure/List.py
@@ -1,3 +1,27 @@
 #simple list example in python
 sim_list= ["Prashid","Jenish","Arun"]
 print(sim_list) #This will print items of the list
+
+#determinig the length of list
+print(len(sim_list))
+
+#indexing the content of list
+print(sim_list[1])
+
+#adding the content on list
+sim_list.append("Test")
+print(sim_list)
+
+#accessing the last element of the list
+print(sim_list[-1])
+
+#using slice operation on list
+print(sim_list[2:4])
+
+#keeping the odd value less than 10 on list
+new_list=[]
+for x in range(0,11):
+    if x % 2 == 1:
+        new_list.append(x)
+print("This are the list of odd number less than 10",new_list)
+
