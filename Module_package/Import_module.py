d89fa7058a78f6f0644e78b6fa269006231fbd4c
prashid98 <prashid.kafle@cotiviti.com>
2022-04-06 15:45:27 +0545

    Working on the module

---
+++ Module_package/Import_module.py
@@ -0,0 +1,5 @@
+from unicodedata import name
+import Module
+
+new_num= Module.module_num_list
+print(list(new_num))
---
+++ Module_package/Module.py
@@ -0,0 +1,3 @@
+#Module is a file containing a set of functions you want to include in your application.
+module_num_list= [x for x in range (1,10)]
+print(list(module_num_list))
