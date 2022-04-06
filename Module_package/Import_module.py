1f30b05b8ebab3d365a8ca2e19eb46ea54a2e343
prashid98 <prashid.kafle@cotiviti.com>
2022-04-06 17:05:24 +0545

    knowledge of modules

--- Module_package/Import_module.py
+++ Module_package/Import_module.py
@@ -1,11 +1,16 @@
-import mod1 as Module
+import mod1 as Module1
+import mod2 as Module2
 #build in module 
 import platform
 
 #import data from the module file
-new_num= Module.module_num_list
+new_num= Module1.module_num_list
 print(list(new_num))
 
 #import data from the buildin module
 sys=dir(platform)
 print(sys)
+
+#importing the 2 different module and having a computation among them
+sum = Module1.module_num_list + Module2.module2_num_list
+print(list(sum))
---
+++ Module_package/mod2.py
@@ -0,0 +1,2 @@
+module2_num_list= [x for x in range(11,20)]
+print(list(module2_num_list))
