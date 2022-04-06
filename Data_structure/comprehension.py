9d4e45ff08418645eddf26b01e0196a3b68ffafc
prashid98 <prashid.kafle@cotiviti.com>
2022-04-06 09:45:17 +0545

    knowledge of comprehension

--- Data_structure/comprehension.py
+++ Data_structure/comprehension.py
@@ -20,3 +20,22 @@ for x in new_list3:
     if x % 2 == 1:
         dictionary_comp[x]= x * 2 
 print("this is the output of dictionary comprenhension:",dictionary_comp)
+
+#the above dictionary program can also be written as following
+dictionary_comp2= {x:x*2 for x in new_list3 if x % 2 == 1}
+print("this is the output of dictionary comprenhension:",dictionary_comp2)
+
+
+#Set comprehension for the even number less than 10
+new_list4 = [x for x in range(1,10)]
+set_comp = set()
+
+for x in new_list4:
+    if x % 2 == 0:
+        set_comp.add(x)
+print(set_comp)
+
+#the above set comprehension program can also be written as
+
+set_comp1 = {x for x in new_list4 if x % 2 == 0}
+print(set_comp1)
