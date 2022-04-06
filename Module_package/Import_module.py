5f8b84f6d3b836ace07d992f1778df46a91b20e7
prashid98 <prashid.kafle@cotiviti.com>
2022-04-06 15:54:47 +0545

    new module initialized for package

--- Module_package/Import_module.py
+++ Module_package/Import_module.py
@@ -1,5 +1,11 @@
-from unicodedata import name
-import Module
+import mod1 as Module
+#build in module 
+import platform
 
+#import data from the module file
 new_num= Module.module_num_list
 print(list(new_num))
+
+#import data from the buildin module
+sys=dir(platform)
+print(sys)
