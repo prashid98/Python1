d3324c99d87655ed54417e8d1d2dd37aa0173ffa
prashid98 <prashid.kafle@cotiviti.com>
2022-04-07 07:35:34 +0545

    working on the f-strings

---
+++ string_formating/String.py
@@ -0,0 +1,11 @@
+#string formating using % operator
+city= "kathmandu"
+zip_add= 44600
+country= "Nepal"
+print("My location is: %d %s, %s" %(zip_add,city,country))
+
+#string operation using the format() operation
+print("Hello {name}, Welcome to {company} ".format(company= "cotiviti", name="Prashid Kafle"))
+
+#string operation using f-string 
+
