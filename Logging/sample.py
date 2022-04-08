a12f8186c7599516e4204e386593a74a59c2a8cb
prashid98 <prashid.kafle@cotiviti.com>
2022-04-08 13:47:36 +0545

    working on logging

---
+++ Logging/sample.py
@@ -0,0 +1,11 @@
+import logging
+
+
+# logging.info('This is an info message')
+# logging.warning('This is a warning message')
+# logging.error('This is an error message')
+# logging.critical('This is a critical message')
+
+  
+logging.basicConfig(level=logging.DEBUG)  
+logging.debug('The dubug message is logged')  
